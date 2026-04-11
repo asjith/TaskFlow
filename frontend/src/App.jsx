6f7a6bcd9e87a1d14137898caf51370257b6f0fd
anusreesjith <anusreesjith@gmail.com>
2026-04-11 21:42:18 +0530

    feat: set up React Router for login and projects pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
 import "./App.css";
 import Login from "./components/Login";
+import ProjectsLayout from "./components/ProjectsLayout";
+import ProjectList from "./components/ProjectList";
+import Project from "./components/Project";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/projects",
+      element: <ProjectsLayout />,
+      children: [
+        {
+          index: true,
+          element: <ProjectList />
+        },
+        {
+          path: ":id",
+          element: <Project />
+        }
+      ]
+    }
+  ]);
   return (
     <div>
-      <Login />
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
